f53077ad63dec11799b1db67fcc6daf02299581e
vishalkrkamat <vishalkrkamat7@gmail.com>
2025-01-08 22:41:11 +0530

    trying tcp implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+fn handle(stream: TcpStream) -> std::io::Result<()> {
+    Ok(())
+}
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        let _ = handle(stream);
+        println!("Connection established");
+    }
+    Ok(())
 }
